8b981474145cbc317457f4b8b1cf02679b23d83a
Isley Griffith <isley@realt.co>
2023-10-01 14:14:30 -0700

    Change userButton theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { UserButton, useAuth } from "@clerk/nextjs";
 import { getLibrary } from "@/utils/supabaseRequests";
-
+import { dark } from "@clerk/themes";
 
 export default function Sidebar({ items }) {
   const { userId, getToken } = useAuth();
@@ -11,20 +11,20 @@ export default function Sidebar({ items }) {
     const loadLibrary = async () => {
       const token = await getToken({ template: "supabase" });
       const libData = await getLibrary({ userId, token });
-      setLibrary(libData)
+      setLibrary(libData);
 
-      console.log("data:",libData)
-      
+      console.log("data:", libData);
     };
 
-    loadLibrary()
+    loadLibrary();
   }, []);
 
   return (
     <div className="h-screen min-w-[256px] p-4 bg-gray-d border-r border-r-[#000000]">
       <div className="absolute bottom-4 left-4">
-        <UserButton />
-        {/* {console.log(library)} */}
+        <div>
+          <UserButton appearance={dark} />
+        </div>
       </div>
     </div>
   );
